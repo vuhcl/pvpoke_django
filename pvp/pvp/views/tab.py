6d3e35ea90c56f7dd2da0179d0a9dbfb3f2a8429
Vu H. Chu-Le <vu@minerva.kgi.edu>
2023-06-30 08:15:15 -0700

    Initial commit for detail tabs
    - base templates for detail tabs
    - detail tab view
    - implemented tabs using hyperscript

---
+++ pvp/pvp/views/tab.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.decorators.http import require_GET
+from django.http import HttpResponse
+import json
+from pvp.models import Format
+from . import HtmxHttpRequest
+
+@require_GET
+def get_tab(request: HtmxHttpRequest, tab:str) -> HttpResponse:
+    pass
