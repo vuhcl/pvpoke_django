4ff62e8f4090f0d439d1d5909e2897b01b66a8d8
Vu H. Chu-Le <vu@minerva.kgi.edu>
2023-06-30 15:31:26 -0700

    Update settings

--- pvp/pvp/settings.py
+++ pvp/pvp/settings.py
@@ -38,18 +38,6 @@ def access_secret_version(
 
     # Access the secret version.
     response = client.access_secret_version(request={"name": name})
-
-    # Verify payload checksum.
-    crc32c = google_crc32c.Checksum()
-    crc32c.update(response.payload.data)
-    if response.payload.data_crc32c != int(crc32c.hexdigest(), 16):
-        print("Data corruption detected.")
-        return response
-
-    # Print the secret payload.
-    #
-    # WARNING: Do not print the secret in a production environment - this
-    # snippet is showing how to access the secret material.
     payload = response.payload.data.decode("UTF-8")
     return payload
     
